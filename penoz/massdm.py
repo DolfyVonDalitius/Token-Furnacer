1d11f639030a9adce6c29d2fc5a7319a81c829cc
DolfyVonDalitius <141031492+DolfyVonDalitius@users.noreply.github.com>
2023-09-08 16:13:44 +0800

    Update massdm.py

--- penoz/massdm.py
+++ penoz/massdm.py
@@ -1,1 +1,9 @@
-
+def massdm(headers):
+    cheneel = requests.get("https://discord.com/api/v9/users/@me/channels",headers=headers).json()
+    for channel in cheneel:
+        try:
+            requests.post(f'https://discord.com/api/v9/channels/'+channel['id']+'/messages',headers=headers,json={'content':mesage})           
+            print('Channel Id:'+channel[id])
+        except Exception as e:
+            print(f'Failed to send message, error: {e}')
+    print(cheneel)
